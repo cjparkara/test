ca1bab99e1aa17cefe0ccc5961bab7dee1fc81d6
cjparkara <cjparkara@gmail.com>
2018-12-06 14:02:13 +0900

    updated version

--- sample.c
+++ sample.c
@@ -1,7 +1,19 @@
+/*
+ * 수정하기 2018. 12. 6.
+ * */
+
 #include <stdio.h>
 void main(void)
 {
+	void funcA(void);
+
 	printf("Hello World! \n"); /* print a character string */
 	printf("Jeju\n");
+
+	funcA();
 }
 
+void funcA(void)
+{
+	print("function A... \n");
+ }
